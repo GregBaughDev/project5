8330a5cc8e308e8d89121aaef90730870047f573
RHYS <62794614+hy-js@users.noreply.github.com>
2021-08-18 12:57:05 +1000

    updates bootstrap UI and pagination

--- public/js/app.js
+++ public/js/app.js
@@ -38,7 +38,6 @@ const apiCallNowPlaying = () => {
 
 
 // TODO: [RD] Add better movie title UI i.e. no text overflow
-
 const getMovies = (page) => {
   apiCallNowPlaying()
     .then((data) => {
@@ -47,67 +46,79 @@ const getMovies = (page) => {
       const movie = movies[i]
       const movieHTML = $('<div class="movie-div">')
         .append(`<h5 class="movie-title" id="${movie.id}">${movie.title}</h5>`)
-        .append(`<a href="/details/${movie.id}"><img src="${image_URL + movie.poster_path}" alt="${movie.title} poster"></a>`)
-        $('#api-test').append(movieHTML);
+        .append(`<a href="/details/${movie.id}"><img src="${image_URL}${movie.poster_path}" alt="${movie.title} poster onerror="this.onerror=''; this.src='./assets/blank.jpg'"></a>`) // If poster load error: load blank.jpg
+        // TODO: Community rating should be from DB
+        .append(`<span id="star" class="rating">★★★★★</span>`);
+        $('#api-content').append(movieHTML);
       }
-      // Change page title and add pagination, if first page: don't include prev button
-      $('#page-title').append(`<h2>Now Playing</h2>`);
-      $("#pages").removeClass("d-none")
-      if(page === 1) return $(".prev-page").addClass("d-none")
+      // Change page title and add pagination, if first or last page: don't include pagination
+      // TODO: Refactor
+      $(".pages").removeClass("d-none")
+      if(data.page === 1){
+        $(".prev-page").addClass("d-none")
+        $('#page').text(`Page ${data.page}/${data.total_pages}`)
+        return
+      }
       $(".prev-page").removeClass("d-none")
+      if(data.page === data.total_pages) return $(".next-page").addClass("d-none")
+      $(".next-page").removeClass("d-none")
+      console.log(data.page)
+      $('#page').text(`Page ${data.page}/${data.total_pages}`)
     })
     .catch((err) => {
-      console.log(err);
-      $('#api-test').append(`<p>${err.responseJSON.status_message}</p>`);
+      console.log("err");
+      $('#api-content').append(`<p>${err.responseJSON.status_message}</p>`);
     })
 }
 
 const searchMovies = (genre_id) => {
   apiCallNowPlaying()
     .then((data) => {
-      $( "#api-test" ).empty();
-      $( "#page-title" ).empty();
-      const movies = data.results
+      $( "#api-content" ).empty();
+      let movies = data.results
       const genreName = Object.keys(genres).find(key => genres[key] === genre_id)
           for(let mov of movies){
               if(mov.genre_ids.includes(genre_id)){
                 const movieHTML = $('<div class="movie-div">')
                 .append(`<h5 class="movie-title">${mov.title}</h5>`)
-                .append(`<a href="/details/${mov.id}"><img src='${image_URL}${mov.poster_path}' alt='${mov.title} poster'></a>`);
-                $('#api-test').append(movieHTML);
+                .append(`<a href="/details/${mov.id}"><img src='${image_URL}${mov.poster_path}' alt='${mov.title} poster onerror="this.onerror=''; this.src='./assets/blank.jpg''></a>`)
+                // TODO: Community rating should be from DB
+                // Unicode stars are placeholder and can be replaced with different rating system/svgs
+                .append(`<span id="star" class="rating">★★★★★</span>`);
+                $('#api-content').append(movieHTML);
             }
         }
         // Change page title and remove pagination
-        $('#page-title').append(`<h2 class="genre-search">Movies by Genre: ${genreName}</h2>`);
-        $("#pages").addClass("d-none")
+        $('#page-title').text(`Movies by Genre: ${genreName}`).addClass("genre-search");
+        $('#page').text(``)
+        $("#next-page").addClass("d-none")
+        $("#prev-page").addClass("d-none")
     })
     .catch((err) => {
       console.log(err);
-      $('#api-test').append(`<p>${err.responseJSON.status_message}</p>`);
+      $('#api-content').append(`<p>${err.responseJSON.status_message}</p>`);
     })
 }
 
 // On document ready the getMovies function is called
 $(document).ready(() => {
   getMovies(page)
-
 });
 
 // Next page
 $(".next-page").click(() => {
   page = page + 1
-  $("#api-test").empty()
-  $("#page-title").empty()
+  $("#api-content").empty()
   getMovies(page)
 })
 
 //  Previous page
 $(".prev-page").click(() => {
   page = page - 1
-  $("#api-test").empty()
-  $("#page-title").empty()
+  $("#api-content").empty()
   getMovies(page)
 })
+
 
 // When a genre is selected from the drop down the following is called
 $('#genre').change((e) => {
@@ -115,3 +126,12 @@ $('#genre').change((e) => {
   searchMovies(genres[e.target.value])
 })
 
+// Hide footer until scroll at bottom
+document.onscroll = function() {
+  if(document.documentElement.scrollTop + window.innerHeight == document.documentElement.scrollHeight)
+  {
+    $("footer").removeClass("d-none")
+  }else{
+    $("footer").addClass("d-none")
+  }
+}
